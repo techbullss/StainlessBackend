4768aa274d68bf2e06b9c25112b3f250aaa65eaa
Maish <bwanmaina2010@gmail.com>
2025-08-06 20:23:10 +0300

    mm

--- StainlesSteel/src/main/java/com/example/StainlesSteel/Configurations/WebMvcConfig.java
+++ StainlesSteel/src/main/java/com/example/StainlesSteel/Configurations/WebMvcConfig.java
@@ -9,7 +9,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // Change the path here if your uploads are saved elsewhere
-        registry.addResourceHandler("https://stainlessbackend-5.onrender.com/uploads/**")
+        registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:uploads/");
     }
 }
